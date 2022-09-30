c9fad7eaad93433806a90ab0f8f530de8346d2c9
mar-cial <cesarmarcialq@gmail.com>
2022-09-30 14:59:20 -0700

    provide a bit more information

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,10 +31,10 @@ const Homepage: NextPage = () => {
   });
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen gap-3 p-4 text-center text-white bg-zinc-900">
+    <div className="flex flex-col items-center justify-center min-h-screen gap-3 p-4 text-center text-white bg-gradient from-zinc-700 to-zinc-900 bg-zinc-900">
       <h1 className="text-5xl font-medium">Suma push notifs testing</h1>
 
-      {`${initialized ? 'Initialized.': 'Not initialized.'} ${isSubscribed ? 'You are subscribed and can receive notifications' : 'You are not subscribed and can\'t receive notifications.'}`}
+      {`${initialized ? 'Initialized.': 'Not initialized.'} ${isSubscribed ? 'You are subscribed and can receive notifications. Lmk if you want to receive one to see how it looks like, it\' still pretty manual just for testing purposes.' : 'You are not subscribed and can\'t receive notifications.'}`}
     </div>
   );
 };
