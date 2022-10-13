ccd8706739bc9bd9bf2a5611114348ec0faeb20e
mar-cial <cesarmarcialq@gmail.com>
2022-10-13 12:56:00 -0700

    feat: working on setting external ids correctly

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,41 +1,40 @@
 import { NextPage } from 'next';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import OneSignalReact from 'react-onesignal';
-import { v4 as uuidv4, v4 } from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 
 const Homepage: NextPage = () => {
   const [initialized, setInitialized] = useState<boolean>(false);
   const [isSubscribed, setIsSubscribed] = useState<boolean>(false);
-  
-  useEffect(() => {
-    OneSignalReact.init({
-      appId: '781727fc-d36e-4468-9c98-40478ae77364',
-      serviceWorkerPath: '/OneSignalSDKWorker.js',
-      welcomeNotification: {
-        title: 'Welcome to Suma Wealth!',
-        messaage: "It's great to see you here. Leave your finances to us!",
-      },
-      persistNotification: false,
-      autoResubscribe: true,
-      
-    }).then(async () => {
+
+  OneSignalReact.init({
+    appId: '781727fc-d36e-4468-9c98-40478ae77364',
+    serviceWorkerPath: '/OneSignalSDKWorker.js',
+    welcomeNotification: {
+      title: 'Welcome to Suma Wealth!',
+      messaage: "It's great to see you here. Leave your finances to us!",
+    },
+    persistNotification: false,
+    autoResubscribe: true,
+  })
+    .then(async () => {
       setInitialized(true);
-      
+
       OneSignalReact.isPushNotificationsEnabled((isEnabled) => {
         if (isEnabled) {
-          console.log("Push notifications are enabled!");
-          OneSignalReact.setExternalUserId(uuidv4().substring(0, 7))
+          console.log('Push notifications are enabled!');
+          OneSignalReact.setExternalUserId("abc-123-abcd");
         } else {
-          console.log("Push notifications are not enabled yet.");
+          console.log('Push notifications are not enabled yet.');
         }
-      })
-      
+      });
+
       try {
         const status = await OneSignalReact.getNotificationPermission();
 
         switch (status) {
           case 'default':
-            OneSignalReact.showSlidedownPrompt({force: true});
+            OneSignalReact.showSlidedownPrompt({ force: true });
             break;
 
           case 'granted':
@@ -51,13 +50,13 @@ const Homepage: NextPage = () => {
         }
       } catch (err) {
         console.error(err);
-        return
+        return;
       }
-    }).catch((err) => {
-      console.error(err)
-      return
     })
-  }, []);
+    .catch((err) => {
+      console.error(err);
+      return;
+    });
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen gap-3 p-4 text-center text-white bg-gradient from-zinc-700 to-zinc-900 bg-zinc-900">
