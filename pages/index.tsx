75db100c8c2ef8b6f1c7a58de03617806796389f
mar-cial <cesarmarcialq@gmail.com>
2022-10-13 12:17:07 -0700

    feat: working on setting external ids correctly

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,38 +1,36 @@
 import { NextPage } from 'next';
 import React, { useEffect, useState } from 'react';
 import OneSignalReact from 'react-onesignal';
-
+import { v4 as uuidv4, v4 } from 'uuid';
 
 const Homepage: NextPage = () => {
   const [initialized, setInitialized] = useState<boolean>(false);
   const [isSubscribed, setIsSubscribed] = useState<boolean>(false);
   
   useEffect(() => {
-    
-
     OneSignalReact.init({
       appId: '781727fc-d36e-4468-9c98-40478ae77364',
-      serviceWorkerPath: '../OneSignalSDKWorker.js',
+      serviceWorkerPath: '/OneSignalSDKWorker.js',
       welcomeNotification: {
         title: 'Welcome to Suma Wealth!',
         messaage: "It's great to see you here. Leave your finances to us!",
       },
       persistNotification: false,
       autoResubscribe: true,
+      
     }).then(async () => {
       setInitialized(true);
       
       OneSignalReact.isPushNotificationsEnabled((isEnabled) => {
         if (isEnabled) {
           console.log("Push notifications are enabled!");
-          OneSignalReact.setExternalUserId("abc-123-abcd")
+          OneSignalReact.setExternalUserId(uuidv4().substring(0, 7))
         } else {
           console.log("Push notifications are not enabled yet.");
         }
       })
       
       try {
-
         const status = await OneSignalReact.getNotificationPermission();
 
         switch (status) {
@@ -53,9 +51,11 @@ const Homepage: NextPage = () => {
         }
       } catch (err) {
         console.error(err);
+        return
       }
     }).catch((err) => {
       console.error(err)
+      return
     })
   }, []);
 
